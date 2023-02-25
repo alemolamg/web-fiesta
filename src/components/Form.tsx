ed3c13d020f359fd0b55b67dda23fbe2f8290f82
Alemol <alemolamg@outlook.com>
2023-02-25 18:07:12 +0100

    UPDATE FOR MAIN

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,7 +4,10 @@ const Form: FC<{ }> = ({ }) => {
     //const classStyle: string = "my-2 px-2 py-1 md:mx-4 md:rounded-xl lg:mx-5 " + className
     return (
         <>
-        
+            <h2 className="text-center uppercase text-2xl ">formulario de asistencia:</h2>
+            <div className="mx-5 text-red-400 text-center">
+                En proceso de creación
+            </div>
         </>
     )
 }
