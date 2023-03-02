dce90860430a060073b74c2958ee39914777b95b
AlemolAMG <alemol@clickferry.com>
2023-03-02 08:25:44 +0100

    PEQUEÑO CAMBIO INSERVIBLE

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://nrihasluaxwszantbcul.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yaWhhc2x1YXh3c3phbnRiY3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzczNDY1OTAsImV4cCI6MTk5MjkyMjU5MH0.oV09PyI_PToc4COK7AvRcICMlGlfK3ThVQe3VzMEcL4";
+const supabaseAnonKey = "IkCbIiY95nIMK44";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient("postgresql://postgres:IkCbIiY95nIMK44@db.nrihasluaxwszantbcul.supabase.co:5432/postgres");
