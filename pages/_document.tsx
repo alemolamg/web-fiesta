d28a627ecc047e5d474be53d9c42aa0f12d40f35
AlemolAMG <alemol@clickferry.com>
2023-03-22 08:10:16 +0100

    comentario para mejorar el notch

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head />
-      <body>
+      <body> {/*style={{backgroundColor:"var(--tarjeta-malaga-bg)"}} */}
         <Main />
         <NextScript />
       </body>
